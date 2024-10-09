a249d6e98c4505597759f172ef6c5d6ffbd9d972
Harun Biswas Rubel <harunbiswasrubel@gmail.com>
2024-10-09 18:28:00 +0600

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    i18n: {
-        defaultLocale: 'en',
-        locales: ['en', 'th'],
-      },
-      images: {
-        domains: ['rayoflightthemes.com'],
-      }, 
-      
-      webpack: (config, { isServer, webpack }) => {
-        return config;
-      }
-
-   
+  i18n: {
+      defaultLocale: 'en',
+      locales: ['en', 'th'],
+  },
+  images: {
+      domains: ['rayoflightthemes.com'],
+  },
+  webpack: (config) => {
+      return config;
+  },
+  reactStrictMode: true,
+    pageExtensions: [
+        "page.jsx",
+        "page.js",
+        // FIXME: Next.js has a bug which does not resolve not-found.page.tsx correctly
+        // Instead, use `not-found.ts` as a workaround
+        // "ts" is required to resolve `not-found.ts`
+        // https://github.com/vercel/next.js/issues/65447
+        "js"
+    ],
 };
 
 export default nextConfig;

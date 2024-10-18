acc69d106b8f5568be9aa58acb3ed91759cf2498
Harun Biswas Rubel <harunbiswasrubel@gmail.com>
2024-10-18 18:22:22 +0600

    update

--- src/components/home/Map.jsx
+++ src/components/home/Map.jsx
@@ -1,30 +1,84 @@
 "use client";
-import React from "react";
-
 import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
+import React from "react";
 
 const containerStyle = {
   width: "100%",
   height: "400px",
-  borderRadius: "15px", // Add border radius here
+  borderRadius: "15px",
   overflow: "hidden",
-  // marginTop: "30px",
 };
 
 const center = {
-  lat: 13.675673947066162, // Default latitude
-  lng: 100.640691795568, // Default longitude
+  lat: 13.68399, // Default latitude
+  lng: 100.64073,
 };
 
-const customMarker = {
-  url: "https://example.com/custom-marker.png",
+const mapOptions = {
+  zoomControl: true, // Enable zoom controls
+  fullscreenControl: true, // Optional: Enable fullscreen control
+  streetViewControl: false, // Optional: Disable street view control
+  mapTypeControl: true, // Optional: Disable map type control
 };
 
 const Map = () => {
+  const [isApiLoaded, setIsApiLoaded] = React.useState(false);
+
+  const customMarker = isApiLoaded
+    ? {
+        url: "/images/logo-1-2.png", // Replace with your custom marker URL
+        scaledSize: new window.google.maps.Size(20, 20), // Adjust the marker size
+      }
+    : null;
+
   return (
-    <LoadScript googleMapsApiKey={"AIzaSyDYNu6tHPINrxFP7-OlbGRTi0mNpn06nMw"}>
-      <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={14}>
-        <Marker position={center} />
+    <LoadScript
+      googleMapsApiKey={"AIzaSyDYNu6tHPINrxFP7-OlbGRTi0mNpn06nMw"}
+      onLoad={() => setIsApiLoaded(true)}
+    >
+      <GoogleMap
+        mapContainerClassName="map-container"
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={13}
+        options={mapOptions} // Add map options for zoom and control
+      >
+        {isApiLoaded && (
+          <Marker
+            position={{
+              lat: 13.66288, // Default latitude
+              lng: 100.60172, // Default longitude
+            }}
+            icon={customMarker}
+          />
+        )}{" "}
+        {isApiLoaded && (
+          <Marker
+            position={{
+              lat: 13.67575, // Default latitude
+              lng: 100.64073, // Default longitude
+            }}
+            icon={customMarker}
+          />
+        )}{" "}
+        {isApiLoaded && (
+          <Marker
+            position={{
+              lat: 13.70565, // Default latitude
+              lng: 100.6005, // Default longitude
+            }}
+            icon={customMarker}
+          />
+        )}{" "}
+        {isApiLoaded && (
+          <Marker
+            position={{
+              lat: 13.6571, // Default latitude
+              lng: 100.67826, // Default longitude
+            }}
+            icon={customMarker}
+          />
+        )}
       </GoogleMap>
     </LoadScript>
   );

3238f53ee727617ee8ea8db348beac4ba3e78f29
Harun Biswas Rubel <harunbiswasrubel@gmail.com>
2024-10-19 11:59:12 +0600

    update responsive issue

--- src/components/home/Banner.jsx
+++ src/components/home/Banner.jsx
@@ -16,7 +16,7 @@ export default function Banner() {
     speed: 1000,
     slidesToShow: 1,
     slidesToScroll: 1,
-    autoplay: true,
+    autoplay: false,
     autoplaySpeed: 3000,
     pauseOnHover: true,
   };

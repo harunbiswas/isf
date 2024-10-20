a9542440b6ac3001ad757f31f0b1280d6d5054d1
Harun Biswas Rubel <harunbiswasrubel@gmail.com>
2024-10-21 00:35:46 +0600

    update

--- src/components/home/Product.jsx
+++ src/components/home/Product.jsx
@@ -59,7 +59,7 @@ export default function Product({ products }) {
               />
               <h4>{product.title}</h4>
               <p>{product?.discription}</p>{" "}
-              <strong className="price">฿{product?.price}</strong>
+              <strong className="price">Pirce: ฿{product?.price}</strong>
             </div>
           ))}
         </Slider>
